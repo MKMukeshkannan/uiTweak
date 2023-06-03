d4ba0d011b1466a4c0395a264929b616043fecc1
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-06-03 19:05:22 +0530

    HOME PAGE COMPLETED DESIGN

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
 
 function SignInButton() {
@@ -9,13 +10,16 @@ function SignInButton() {
   if (session && session?.user) {
     return (
       <>
-        <div className="flex flex-row">
-          <p className="text-4xl font-medium text-gray-900 dark:text-white">
-            {session?.user?.name}{" "}
-          </p>{" "}
+        <div className="flex flex-row gap-8 items-center">
+          <p className="block cursor-default	  text-2xl font-bold	text-[#7ca8a2]">
+            Welcome !!
+            <Link href="/profile" className="pl-2 hover:text-[#077b79]">
+              {session?.user?.name.toUpperCase()}
+            </Link>
+          </p>
           <button
             onClick={() => signOut()}
-            className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            className=" text-[#d3f1e9] bg-[#077b79] focus:ring-4 focus:ring-[#7ca8a2] font-medium rounded-lg text-sm px-5 py-2.5 "
           >
             SignOut
           </button>
@@ -27,7 +31,7 @@ function SignInButton() {
     <div>
       <button
         onClick={() => signIn()}
-        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        className=" text-[#d3f1e9] bg-[#077b79] focus:ring-4 focus:ring-[#7ca8a2] ont-medium rounded-lg text-sm px-5 py-2.5 "
       >
         SignIn
       </button>
